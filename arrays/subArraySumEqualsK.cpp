59d88fc9f11fcacb7466fcf8ae5d942ac16f4950
ishita <ishitabansal0806@gmail.com>
2026-06-17 23:45:27 +0530

    Add solution for Subarray Sum Equals K using prefix sum

---
+++ arrays/subArraySumEqualsK.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int subarraySum(vector<int>& nums, int k) {
+    unordered_map<int, int> mp;
+    mp[0] = 1;
+
+    int sum = 0, count = 0;
+
+    for (int num : nums) {
+        sum += num;
+        if (mp.find(sum - k) != mp.end()) {
+            count += mp[sum - k];
+        }
+        mp[sum]++;
+    }
+    return count;
+}
